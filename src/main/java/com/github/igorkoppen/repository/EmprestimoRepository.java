4d603dec05b8fc73fba6019b6bf4928c6c0cb779
Igor Pasqualino <igorpasqualino@gmail.com>
2024-10-18 10:35:24 -0300

    Repository's for the entity's

--- src/main/java/com/github/igorkoppen/repository/EmprestimoRepository.java
+++ src/main/java/com/github/igorkoppen/repository/EmprestimoRepository.java
@@ -1,9 +1,15 @@
 package com.github.igorkoppen.repository;
 
 import com.github.igorkoppen.model.Emprestimo;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.hibernate.reactive.panache.PanacheRepository;
+import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
+
 
 @ApplicationScoped
 public class EmprestimoRepository implements PanacheRepository<Emprestimo> {
+
+    public Uni<Long> countByLivroId(Long livroId) {
+        return count("livro.id", livroId);
+    }
 }
--- src/main/java/com/github/igorkoppen/repository/LivroRepository.java
+++ src/main/java/com/github/igorkoppen/repository/LivroRepository.java
@@ -1,7 +1,7 @@
 package com.github.igorkoppen.repository;
 
 import com.github.igorkoppen.model.Livro;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.hibernate.reactive.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
