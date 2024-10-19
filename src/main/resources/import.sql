268c5a36b486398a536b5605560e4716f06152b5
Igor Pasqualino <igorpasqualino@gmail.com>
2024-10-19 01:26:44 -0300

    Fix imports sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,21 +1,9 @@
-INSERT INTO tb_autor (id, nome, biografia) VALUES
-                                               (1, 'J.R.R. Tolkien', 'Autor de O Senhor dos Anéis'),
-                                               (2, 'George Orwell', 'Autor de 1984'),
-                                               (3,'Rick Riordan','Autor de Percy Jackson');
+INSERT INTO tb_autor (nome, biografia) VALUES ('J.R.R. Tolkien', 'Autor de O Senhor dos Anéis'), ('George Orwell', 'Autor de 1984'), ('Rick Riordan','Autor de Percy Jackson');
 
-INSERT INTO tb_categoria (id, nome) VALUES
-                                        (1, 'Ficção Científica'),
-                                        (2, 'Fantasia');
+INSERT INTO tb_categoria (nome) VALUES ('Ficção Científica'), ('Fantasia');
 
-INSERT INTO tb_livro (id, titulo, disponivel, autorId, categoriaId) VALUES
-                                                                        (1, 'O Senhor dos Anéis', true, 1, 2),
-                                                                        (2, '1984', true, 2, 1),
-                                                                        (3, 'Percy Jackson e os Olimpianos', true, 3, 2);
+INSERT INTO tb_livro (titulo, disponivel, autorId, categoriaId) VALUES ('O Senhor dos Anéis', true, 1, 2), ('1984', true, 2, 1), ('Percy Jackson e os Olimpianos', true, 3, 2);
 
-INSERT INTO tb_usuario (id, nome, email, senha, tipo) VALUES
-                                                          (1, 'Alice Silva', 'alice@email.com', 'senha123', 'PROFESSOR'),
-                                                          (2, 'João Sousa', 'joao@email.com', 'senha456', 'CRIANCA');
+INSERT INTO tb_usuario (nome, email, senha, tipo) VALUES ('Alice Silva', 'alice@email.com', 'senha123', 'PROFESSOR'), ('João Sousa', 'joao@email.com', 'senha456', 'CRIANCA');
 
-INSERT INTO tb_emprestimo (id, livroId, usuarioId, dataEmprestimo, dataDevolucao, dataDevolvido) VALUES
-                                                                                                     (1, 1, 1, '2024-10-01', '2024-10-10', NULL),
-                                                                                                     (2, 2, 2, '2024-10-05', '2024-10-15', NULL);
+INSERT INTO tb_emprestimo (livroId, usuarioId, dataEmprestimo, dataDevolucao, dataDevolvido) VALUES (1, 1, '2024-10-01', '2024-10-10', NULL), (2, 2, '2024-10-05', '2024-10-15', NULL);
