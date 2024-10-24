530d6c52033fa92f8b3cfd8ed4dc93b36417e3d0
Igor Pasqualino <igorpasqualino@gmail.com>
2024-10-24 13:46:41 -0300

    Logica se existe por categoria no repo

--- src/main/java/com/github/igorkoppen/repository/LivroRepository.java
+++ src/main/java/com/github/igorkoppen/repository/LivroRepository.java
@@ -2,8 +2,15 @@ package com.github.igorkoppen.repository;
 
 import com.github.igorkoppen.model.Livro;
 import io.quarkus.hibernate.reactive.panache.PanacheRepository;
+import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class LivroRepository implements PanacheRepository<Livro> {
+
+    public Uni<Boolean> existsByCategoriaId(Long categoriaId) {
+        return find("categoriaId", categoriaId).firstResult()
+                .onItem().ifNotNull().transform(livro -> true)
+                .onItem().ifNull().continueWith(false);
+    }
 }
